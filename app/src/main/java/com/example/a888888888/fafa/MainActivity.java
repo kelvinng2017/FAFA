29aeab9fe6021dc8cb2979e418ef2ee20f3a3e1b
kelvinngshuhui <kelvinngshuhui@gmail.com>
2017-07-26 22:20:08 +0800

    在MainAcitivity增加退出按鈕的功能

--- app/src/main/java/com/example/a888888888/fafa/MainActivity.java
+++ app/src/main/java/com/example/a888888888/fafa/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.a888888888.fafa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.util.Random;
+
+import static com.example.a888888888.fafa.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
     TextView numberOne,numberTwo,numberThree;
     Random ran = new Random();
+    Button angryButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -19,5 +24,18 @@ public class MainActivity extends AppCompatActivity {
         numberOne.setText(Integer.toString(ran.nextInt(10)));
         numberTwo.setText(Integer.toString(ran.nextInt(10)));
         numberThree.setText(Integer.toString(ran.nextInt(10)));
+        angryButton = (Button) findViewById(R.id.angry_btn);
+        angryButton.setOnClickListener(ButtonExit);
     }
+
+    private Button.OnClickListener ButtonExit=
+            new  Button.OnClickListener(){
+                @Override
+                public void  onClick(View v){
+                    finish();
+                }
+
+            };
+
+
 }
